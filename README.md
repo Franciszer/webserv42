600e5d0f42264b1b4f7269be79e9ca6fad6e3443
Francois THIERREE <frthierr@e2r5p20.42.fr>
2020-10-28 12:57:46 +0100

    repo created

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # webserv42
 A webserver in c++
+
+Postman for http requests tests
+
+TCP/IP
+https://medium.com/from-the-scratch/http-server-what-do-you-need-to-know-to-build-a-simple-http-server-from-scratch-d1ef8945e4fa
+
+HTML:
+https://www.tutorialspoint.com/http/index.htm
---
+++ includes/webserv.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   webserv.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frthierr <frthierr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/27 14:42:10 by frthierr          #+#    #+#             */
+/*   Updated: 2020/10/27 18:12:37 by frthierr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef		WEBSERV_HPP
+
+# define	WEBSERV_HPP
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <iostream>
+#include <string>
+
+#endif
